3e81bddaa8e3ac70d824cba2e879f4c4df760c7b
miguelrktdev <contatomigueldev@gmail.com>
2026-05-21 15:58:22 -0300

    ✨ Feat: Create user register route

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,5 +1,8 @@
 import { app } from "@/app.ts"
 import { env } from "@/lib/env.ts"
+import { userRoutes } from "@/routes/index.ts"
+import { StatusCodes } from "http-status-codes"
+import z, { ZodError } from "zod"
 
 app.get('/health', async () => {
   return {
@@ -8,8 +11,22 @@ app.get('/health', async () => {
     timeStamp: new Date().toISOString(),
   }
 })
+app.register(userRoutes, { prefix: "/user" })
 
-const start = () => {
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(StatusCodes.BAD_REQUEST).send({
+      success: false,
+      message: z.treeifyError(error)
+    })
+  }
+  return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+    success: false,
+    message: "INTERNAL_SERVER_ERROR"
+  })
+})
+
+const start = async () => {
   try {
     app.listen({
       port: env.PORT,
