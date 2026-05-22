75da496c5ed8ac5367523da561128eff91262b29
miguelrktdev <contatomigueldev@gmail.com>
2026-05-22 16:14:21 -0300

    🩹 Hotfix: Some fixes

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -4,39 +4,39 @@ import { userRoutes } from "@/routes/index.ts"
 import { StatusCodes } from "http-status-codes"
 import z, { ZodError } from "zod"
 
-app.get('/health', async () => {
-  return {
-    status: 'ok',
-    service: "User Service",
-    timeStamp: new Date().toISOString(),
-  }
+app.get("/health", async () => {
+    return {
+        status: "ok",
+        service: "User Service",
+        timeStamp: new Date().toISOString(),
+    }
 })
 app.register(userRoutes, { prefix: "/user" })
 
 app.setErrorHandler((error, request, reply) => {
-  if (error instanceof ZodError) {
-    return reply.status(StatusCodes.BAD_REQUEST).send({
-      success: false,
-      message: z.treeifyError(error)
+    if (error instanceof ZodError) {
+        return reply.status(StatusCodes.BAD_REQUEST).send({
+            success: false,
+            message: z.treeifyError(error),
+        })
+    }
+    return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        success: false,
+        message: "INTERNAL_SERVER_ERROR",
     })
-  }
-  return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-    success: false,
-    message: "INTERNAL_SERVER_ERROR"
-  })
 })
 
 const start = async () => {
-  try {
-    app.listen({
-      port: env.PORT,
-      host: "0.0.0.0",
-    })
-    console.log(`HTTP Server running`)
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
+    try {
+        app.listen({
+            port: env.PORT,
+            host: "0.0.0.0",
+        })
+        console.log(`HTTP Server running`)
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
 }
 
 start()
