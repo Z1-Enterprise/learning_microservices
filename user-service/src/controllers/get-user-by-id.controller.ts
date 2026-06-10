360cbad66e78e99577d0037d6ccb0d491d653065
miguelrktdev <contatomigueldev@gmail.com>
2026-06-10 16:31:39 -0300

    🩹 Fix: Fix user param on get user by id controller

--- user-service/src/controllers/get-user-by-id.controller.ts
+++ user-service/src/controllers/get-user-by-id.controller.ts
@@ -5,15 +5,13 @@ import { GetUserByIdService } from "@/services/get-user-by-id.service.ts";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { StatusCodes } from "http-status-codes";
 
+interface Request {
+  id: string
+}
 
-export const getUserById = async (request: FastifyRequest, reply: FastifyReply) => {
+export const getUserById = async (request: FastifyRequest<{ Params: Request }>, reply: FastifyReply) => {
   try {
-    const { success, data, error } = USER_SCHEMA.GET_USER_BY_ID.safeParse(request.body)
-    if (!success) {
-      const issue = error.issues[0]
-      return reply.status(400).send({ success: false, message: issue.message })
-    }
-    const { id } = data
+    const { id } = request.params
     const { user } = await new GetUserByIdService(new UserRepository()).execute({ id })
     return reply.status(StatusCodes.OK).send({ success: true, user })
   } catch (error) {
